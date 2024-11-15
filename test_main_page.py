b4fa6b37f538f8b22ed664c3f9441aac79d00477
garnayavs <garnayavs@gmail.com>
2024-11-15 17:18:25 +0300

    add base & main page

--- test_main_page.py
+++ test_main_page.py
@@ -1,10 +1,7 @@
-from selenium.webdriver.common.by import By
-import time
+from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    time.sleep(5)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-    time.sleep(5)
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
